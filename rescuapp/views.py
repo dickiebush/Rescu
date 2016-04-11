77b075355d4dfb71f6fb294b6d59f1815b04cd78
Richard Bush <rjb2@princeton.edu>
2016-04-11 11:57:42 -0400

    updated views.py

--- rescuapp/views.py
+++ rescuapp/views.py
@@ -1,6 +1,32 @@
 from app import myapp
 
+## Routes to the splash page 
+## Splash page allows users to log in or sign up
 @myapp.route('/')
 @myapp.route('/index')
-def index():
+def home():
     return "Hello, World!"
+
+## Routes to the login page
+@myapp.route('/login')
+def login():
+    return "Here we log in"
+
+## Routes to the signup page
+@myapp.route('/signup')
+def signup():
+    return "Here we sign up"
+
+
+## Routes to the order page
+@myapp.route('/order')
+def order():
+    return "Here we make orders"
+
+
+## Routes to the FAQ Page
+@myapp.route ('/FAQ')
+def faq():
+    return "Here is a list of FAQ"
+
+
