fddd7fcb6f3b2acf153fec3b97cb47220af32c15
Richard Bush <rjb2@princeton.edu>
2016-04-11 11:43:14 -0400

    first commit

---
+++ rescuapp/views.py
@@ -0,0 +1,6 @@
+from app import myapp
+
+@myapp.route('/')
+@myapp.route('/index')
+def index():
+    return "Hello, World!"
---
+++ run.py
@@ -0,0 +1,4 @@
+#!flask/bin/python
+
+from app import myapp
+myapp.run(debug=True)
