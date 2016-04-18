1eb55e793d2c34b0db1bdb6c50a05d5c5761cdd1
Richard Bush <rjb2@princeton.edu>
2016-04-18 11:51:12 -0400

    updated requirements.txt for heroku deployment

--- rescuapp/views.py
+++ rescuapp/views.py
@@ -1,4 +1,4 @@
-from rescuapp import myapp, db
+from rescuapp import myapp, db, lm
 from .forms import LoginForm, SignUpForm, OrderForm 
 from flask.ext.login import login_user, logout_user, current_user, login_required 
 from .models import User, Order
@@ -8,15 +8,52 @@ from flask import make_response, render_template, request, redirect, flash, sess
 ## Splash page allows users to log in or sign up
 @myapp.route('/')
 @myapp.route('/index')
-def home():
-    return render_template('splash.html')
+def splash():
+
+    ## login form in case of reroute 
+    form = LoginForm()
+
+    ## if user has seen this before, take them to the login page 
+    if request.cookies.get('rescuSplash') == 'yes':
+        print("cookie confirmed")
+        return redirect('/login')
+    else:
+        resp = make_response(render_template('splash.html'))
+        resp.set_cookie('rescuSplash', 'yes')   
+        return resp
 
 ## Routes to the login page
-@myapp.route('/login')
+@myapp.route('/login', methods=['GET','POST'])
 def login():
 
-    ## LEFTOFF -- Handle Login backend processing
+    ## if user is cached and already logged in, take them to order page 
+    if g.user is not None and g.user.is_authenticated:
+        return order()
+
     form = LoginForm()
+
+    ## if sending a post request and form validates
+    if form.validate_on_submit():
+        ## query into database looking for user with email name
+        user = User.query.get(form.email.data)
+        ## (if a user exists)
+        if user: 
+            ## verify password 
+            if form.password.data == user.password:
+                if form.remember_me.data:
+                    session['remember_me'] = True
+                    login_user(user, remember=True)
+                else:
+                    login_user(user, remember=False)
+                ## succesful login
+                return order()
+            ## wrong password 
+            else: 
+                return render_template('login.html', form=form, msg1="Incorrect password")
+        ## no user 
+        else: 
+            return render_template('login.html', form=form, msg2="Please enter a valid email")
+
     return render_template('login.html', form=form)
 
 ## Routes to the signup page
@@ -35,4 +72,17 @@ def order():
 @myapp.route ('/faq')
 def faq():
     return render_template('FAQ.html')
+
+##############################
+#     Helper functions       #
+##############################
+@lm.user_loader
+def load_user(id):
+    return User.query.get(id)
+
+@myapp.before_request
+def before_request():
+    g.user = current_user
+
+
 
